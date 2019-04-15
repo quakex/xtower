3b858fd153d60c53ee4af6a3b2707bafab459d9d
quakex <iquakex@gmail.com>
2019-04-15 15:30:44 +0800

    Replace sign_up template with rails f.xxxx format, and add input validates and error notifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/sign_up'
   get 'static_pages/sign_in'
+  get 'sign_up', to: 'users#new'
+  post 'sign_up', to: 'users#create'
+
+  resources :users
 end
